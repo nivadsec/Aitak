e2f69a660107fca2e43288a22c36954ddef26297
Nivad Sec <nivadsec@gmail.com>
2025-10-14 21:26:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2091994256-87b5d",
+  "appId": "1:998428394933:web:31bae8e6d435c18dd7c5fc",
+  "apiKey": "AIzaSyBidPjln9hvtUJD6_px3aurHhrHsjzHKCQ",
+  "authDomain": "studio-2091994256-87b5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "998428394933"
+};
