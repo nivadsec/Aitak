e98d1e5838574f98984a5ca3c216c59055077464
Nivad Sec <nivadsec@gmail.com>
2025-10-17 15:58:09 +0000

    FirebaseError@https://6000-firebase-studio-1760475652520.cluster-lu4mup4

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,5 @@ export const firebaseConfig = {
   "appId": "1:998428394933:web:31bae8e6d435c18dd7c5fc",
   "apiKey": "AIzaSyBidPjln9hvtUJD6_px3aurHhrHsjzHKCQ",
   "authDomain": "studio-2091994256-87b5d.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "998428394933"
 };
