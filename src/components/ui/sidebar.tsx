5eaae934bb2bf41b220928daea2341904f1224f7
Nivad Sec <nivadsec@gmail.com>
2025-10-17 16:00:24 +0000

    این چرا اینجوریه ، لطفاً درستش بکن

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -191,12 +191,7 @@ const Sidebar = React.forwardRef<
             <SheetContent
               data-sidebar="sidebar"
               data-mobile="true"
-              className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
-              style={
-                {
-                  "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
-                } as React.CSSProperties
-              }
+              className="bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
               side={side}
             >
               <SheetHeader className="p-4 border-b">
